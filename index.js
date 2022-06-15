322ba244ce9b984bcdb56d215f04122bc09e9c9a
Kyle <ktvitale7@gmail.com>
2022-06-14 20:32:06 -0400

    commit tree 6b4b83c25c566ee4321b22679a5535fe2d76eb5b

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+
+  function isPalindrome(word) {
+    // Reversed string variable 
+    let reversed = word.split('').reverse().join('')
+    // If statement to chack if true or fale
+    if(reversed === word) {return true
+  
+    }else return false
+    
+  }
+  // Call function to get result true or false
+  isPalindrome('racecar')
+
 
 /* 
-  Add your pseudocode here
+  Reverse string 
 */
 
 /*
-  Add written explanation of your solution here
+  isPalindrome takes in a string. 
+   Create a variable that reverses the string.
+   If the variable is the same backwards, it returns true.
+   Else the variable will be false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +33,14 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("toy"));
 }
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("eye"));
+
+
 
 module.exports = isPalindrome;
